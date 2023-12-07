898e9ac7ca5b494521d9453ab56687bf49c764d4
Viktor <onnionboy@gmail.com>
2023-12-07 15:18:41 +0300

    db create

---
+++ db/seeders/20231207115710-Themes.js
@@ -0,0 +1,20 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Themes',
+      [
+        {
+          title: 'Animals',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Themes', null, {});
+  },
+};
