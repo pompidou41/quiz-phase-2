1f9e15f39c77e57135c53e3683c8edbbb7420ca6
pompidou41 <musaev5115@gmail.com>
2023-12-08 12:08:20 +0300

    style xmas

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,32 +4,35 @@ const Layout = require('./Layout');
 function NavBar({ user }) {
   console.log(user, 32);
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          quiz
-        </a>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/">
-                Главная
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/category">
-                Категории
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/category">
-                {user && user.name}
-              </a>
-            </li>
-          </ul>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            quiz
+          </a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/">
+                  Главная
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/category">
+                  Категории
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/category">
+                  {user && user.name}
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <div className="xmas"></div>
+    </>
   );
 }
 
